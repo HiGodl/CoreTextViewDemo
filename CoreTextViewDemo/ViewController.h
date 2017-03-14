b570f4d998a3cbfad21242be8fbf2463e12607be
jyLu <l7022995@126.com>
2017-03-14 17:43:58 +0800

    init
    init

---
+++ CoreTextViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreTextViewDemo
+//
+//  Created by jyLu on 2017/3/14.
+//  Copyright © 2017年 jyLu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreTextViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreTextViewDemo
+//
+//  Created by jyLu on 2017/3/14.
+//  Copyright © 2017年 jyLu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
